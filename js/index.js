f43c8534f0e1fe37db614ee2709794913fe77ed2
Wojciech Pietrzak <wojciechpietrzak2003@gmail.com>
2023-02-05 12:06:23 +0100

    add show random answer on click function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 const input = document.querySelector(".app__input");
 const ball = document.querySelector(".app__img");
 const result = document.querySelector(".app__result");
+const answers = [
+	"yes",
+	"no",
+	"count on it",
+	"may be",
+	"ask again",
+	"no doubt",
+	"absolutely",
+	"go for it",
+	"wait for it",
+	"not now",
+	"cannot tell now",
+	"very likely",
+];
 
 const ballAnimation = () => {
 	ball.classList.add("shake-animation");
@@ -9,6 +23,15 @@ const ballAnimation = () => {
 	}, 1000);
 };
 
+const showAnswer = () => {
+	const answer = answers[Math.floor(Math.random() * answers.length)];
+	result.textContent = "";
+	setTimeout(() => {
+		result.textContent = answer;
+	}, 1000);
+};
+
 ball.addEventListener("click", () => {
 	ballAnimation();
+	showAnswer();
 });
